9d554c23ba0e1e310aeddd693d8b07e5c4d083d2
rafabcanedo <rafaitecanedo@gmail.com>
2022-06-26 23:46:40 -0300

    tela homer

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,6 +3,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri:'https://api-sa-east-1.graphcms.com/v2/cl4q7odd4397p01z4dpmhdifj/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+     'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
